c450493cfa0ff7a46b3be4699632ce6c4b9bfb4a
Etienne Caron <etienne.caron@gmail.com>
2016-07-05 21:05:12 -0400

    Very basic outline of the Android lifecycle

--- app/src/main/java/com/pitonneux/android/learningcircleproject/MainActivity.java
+++ app/src/main/java/com/pitonneux/android/learningcircleproject/MainActivity.java
@@ -1,13 +1,65 @@
 package com.pitonneux.android.learningcircleproject;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXAMPLE_KEY = "EXAMPLE_KEY";
+
+    private int exampleState ;
+
+    private Button button;
+
+    // Here's a comment.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+
+        if( savedInstanceState != null ) {
+            // restore the state here.
+            exampleState = savedInstanceState.getInt(EXAMPLE_KEY);
+        }
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    // Activity actually "running"
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(EXAMPLE_KEY, 42);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
 }
