8aa16186588a923cfd99edd5d5286a69a68b5337
Jonathan Lahue <jonathanlahue@gmail.com>
2016-07-09 22:34:29 -0400

    removed example code from MainActivity

--- app/src/main/java/com/pitonneux/android/learningcircleproject/MainActivity.java
+++ app/src/main/java/com/pitonneux/android/learningcircleproject/MainActivity.java
@@ -1,35 +1,16 @@
 package com.pitonneux.android.learningcircleproject;
 
-import android.os.PersistableBundle;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String EXAMPLE_KEY = "EXAMPLE_KEY";
-
-    private int exampleState ;
-
-    private Button button;
-
-    // Here's a comment.
-    // Here's another comment.
-
-//    Here's another comment.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button);
-
-        if( savedInstanceState != null ) {
-            // restore the state here.
-            exampleState = savedInstanceState.getInt(EXAMPLE_KEY);
-        }
     }
 
     @Override
@@ -52,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putInt(EXAMPLE_KEY, 42);
     }
 
     @Override
